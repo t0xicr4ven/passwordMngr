9e678bb84f2c764b6f455869dd34cb99197bbbbc
T0xicR4ven <matty.bremner@protonmail.com>
2022-09-12 14:08:14 +1000

    change script to take sys args

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# PASSWORD MANAGER
+# PASSWORD Tools
+
+## Usage
+`
+python main.py [operation] [arg1] [arg2]
+`
+
+`bash
+Operations
+    gen - will generate password if no arg1 it will default to a len of 16
+    eg: python main.py gen 12
+        your password is: 1dE3Gyui-1@v
+
+`
+
--- main.py
+++ main.py
@@ -1,62 +1,19 @@
 import random
+import sys
 import string
 
-print("""
-██████╗  █████╗ ███████╗███████╗██╗    ██╗ ██████╗ ██████╗ ██████╗     ███╗   ███╗ █████╗ ███╗   ██╗ █████╗  ██████╗ ███████╗██████╗ 
-██╔══██╗██╔══██╗██╔════╝██╔════╝██║    ██║██╔═══██╗██╔══██╗██╔══██╗    ████╗ ████║██╔══██╗████╗  ██║██╔══██╗██╔════╝ ██╔════╝██╔══██╗
-██████╔╝███████║███████╗███████╗██║ █╗ ██║██║   ██║██████╔╝██║  ██║    ██╔████╔██║███████║██╔██╗ ██║███████║██║  ███╗█████╗  ██████╔╝
-██╔═══╝ ██╔══██║╚════██║╚════██║██║███╗██║██║   ██║██╔══██╗██║  ██║    ██║╚██╔╝██║██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══╝  ██╔══██╗
-██║     ██║  ██║███████║███████║╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝    ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║
-╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝     ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═
+# print("""
+# ██████╗  █████╗ ███████╗███████╗██╗    ██╗ ██████╗ ██████╗ ██████╗     ███╗   ███╗ █████╗ ███╗   ██╗ █████╗  ██████╗ ███████╗██████╗ 
+# ██╔══██╗██╔══██╗██╔════╝██╔════╝██║    ██║██╔═══██╗██╔══██╗██╔══██╗    ████╗ ████║██╔══██╗████╗  ██║██╔══██╗██╔════╝ ██╔════╝██╔══██╗
+# ██████╔╝███████║███████╗███████╗██║ █╗ ██║██║   ██║██████╔╝██║  ██║    ██╔████╔██║███████║██╔██╗ ██║███████║██║  ███╗█████╗  ██████╔╝
+# ██╔═══╝ ██╔══██║╚════██║╚════██║██║███╗██║██║   ██║██╔══██╗██║  ██║    ██║╚██╔╝██║██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══╝  ██╔══██╗
+# ██║     ██║  ██║███████║███████║╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝    ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║
+# ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝     ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═
 
-""")
+# """)
 
-
-## password generator
-password = ""
-"""
-1. just lower case no numbers
-2. lowe case and numbers
-3. upper/lower and numbers
-4. upper/lower/numbers and special chars
-"""
-def gen_passwd(lenOfPass,complexOfPass) -> string:
-    the_passwd = ""
-    # default complex to 4 if the input was incorrect
-    if  isinstance(complexOfPass, int):
-        if complexOfPass == 1:
-            the_passwd = easy_passwd(lenOfPass)
-        elif complexOfPass == 2:
-            the_passwd = med_passwd(lenOfPass)
-        elif complexOfPass == 3:
-            the_passwd = hard_passwd(lenOfPass)
-        else:
-            the_passwd = very_hard_passwd(lenOfPass)
-    else:
-        the_passwd = very_hard_passwd(lenOfPass)
-
-    return the_passwd 
-
-# easy pass generation
-def easy_passwd(lenOfPass):
-    the_passwd = ""
-    for _ in range(lenOfPass):
-        the_passwd += "".join(random.choice(string.ascii_lowercase))
-    return the_passwd
-
-# med pass generation
-def med_passwd(lenOfPass):
-    the_passwd = ""
-    for _ in range(lenOfPass):
-        the_passwd += "".join(random.choice(string.ascii_lowercase + string.digits))
-    return the_passwd
-
-# hard pass generation
-def hard_passwd(lenOfPass):
-    the_passwd = ""
-    for _ in range(lenOfPass):
-        the_passwd += "".join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits))
-    return the_passwd
+def gen_passwd(lenOfPass) -> string:
+    return very_hard_passwd(lenOfPass)
 
 # Vhard pass generation
 def very_hard_passwd(lenOfPass):
@@ -66,14 +23,27 @@ def very_hard_passwd(lenOfPass):
     return the_passwd
 
 
-length_of_password = int(input("How long would you like your password: "))
-complex_passwd = ''
-while complex_passwd == '':
-    complex_passwd = input("How difficult would you like your password? 1:easy, 2:med, 3:hard, 4:very hard:[Default is 4]:  ")
+### possible for a switch statment here
+# check to see if use has put in any args
+if len(sys.argv) >= 2:
+    # if user want to make a password
+    if sys.argv[1] == "gen":
+        #user wants to make a password, need to get length
+        if len(sys.argv) == 3:
+           print("Your Password is: ")
+           print(gen_passwd(int(sys.argv[2])))
+        else:
+            print("you have not entered length of password it will now default to 16")
+            print("Your Password is: ")
+            print(gen_passwd(16))
+    else:
+        print("something went wrong or number inputed")
+else:
+    #no arguments entered, tell user to try again
+    print("No arguments entered, please try again. or enter HELP as arg for a list of uses")
+
+
+## password generator
 
 
 
-new_word = gen_passwd(length_of_password, complex_passwd)
-
-print(new_word)
-print(len(new_word))
